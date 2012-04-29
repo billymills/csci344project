4d43838f0254bc02b07a32636ddc13292e0eb789
Billy Mills <billymills@Billys-MacBook-Pro.local>
2012-04-29 15:31:49 -0400

    stopping point commit

--- app/app.js
+++ app/app.js
@@ -17,6 +17,22 @@ var wedge = require('./routes/word/wedge');
 var highland = require('./routes/word/highland');
 var app = module.exports = express.createServer();
 
+//var cf = require('./cloudfoundry.js');
+
+//var redis_port = cf.redis?cf.redis.credentials.port:6379;
+//var redis_host = cf.redis?cf.redis.credentials.host:'localhost';
+//var redis_password = cf.redis?cf.redis.credentials.password:undefined;
+
+//console.log(redis_port);
+//console.log(redis_host);
+//console.log(redis_password);
+
+//if(cf.runningInTheCloud()) {
+//	client.auth(redis_password);
+//}
+
+
+
 var runTwitter = require('./twitter.js');
 //var runSpotter = require('./spotter.js');
 var t = new runTwitter();
@@ -61,6 +77,8 @@ app.get('/word/pisgah', pisgah.index);
 app.get('/word/craggie', craggie.index);
 app.get('/word/wedge', wedge.index);
 app.get('/word/highland', highland.index);
+
+//app.listen(cf.port || 3000);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
